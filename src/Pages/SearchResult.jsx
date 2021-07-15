a25565e45b1303ab8f676ec99a730474eb8f5cf2
ajo <ajoalex012@gmail.com>
2021-07-15 12:15:04 +0530

    search result page ui first phase

---
+++ src/Pages/SearchResult.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavSub from '../Components/NavSub/NavSub'
+import ResultSection from '../Components/ResultSection/ResultSection'
+import Footer from '../Components/Footer/Footer'
+function SearchResult() {
+    return (
+        <div>
+            <NavSub />
+            <ResultSection />
+            <Footer />
+        </div>
+    )
+}
+
+export default SearchResult
